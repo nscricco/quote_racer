9d97617b12e064f019f2dd838016e401746c91b4
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-28 18:59:15 -0700

    adds timer spec: capture start time

--- spec/scripts/timerSpec.js
+++ spec/scripts/timerSpec.js
@@ -1,5 +1,9 @@
 describe("Timer", function(){
 	it("should initialize an undefined start time", function(){
-		expect(timer.startTime).toBeUnDefined();
+		expect(timer.startTime).toBeNull();
 	});
+	it("should capture a start time", function(){
+		timer.startIt();
+		expect(timer.startTime).toEqual(new Date());
+	})
 });
