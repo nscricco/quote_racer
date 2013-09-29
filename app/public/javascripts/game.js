ca63259e7432037b473aa1e7f6722f1708bfa0da
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-28 21:56:29 -0700

    updates game to meet spec

--- app/public/javascripts/game.js
+++ app/public/javascripts/game.js
@@ -1,5 +1,16 @@
 var game = {
 	getGameData: function(container){
-		return container.querySelector("#gameData").innerText;
+		return container.querySelector('#gameData').innerText;
+	},
+	startButtonHandler: function(container){
+		timer.startIt();
+		container.querySelector('#gameInput').focus();
 	}
+}
+
+function gameEvents(container){
+	gameStartButton = container.querySelector('#gameStartButton')
+	gameStartButton.addEventListener('click', function(){
+		game.startButtonHandler(container);
+	});
 }
