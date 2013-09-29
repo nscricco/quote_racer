eaee49e2a605c7aee038bd73e0dcb470ca2d5361
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-29 16:14:35 -0700

    adds movable spec: update style of element in order to move it across the screen

--- spec/scripts/movableSpec.js
+++ spec/scripts/movableSpec.js
@@ -1,8 +1,18 @@
 describe("movable", function(){
-	describe("positions", function(){
-		it("should define the number of positions as the number of words in the phrase", function(){
-			tracker.correct = "There goes Ted Williams, the greatest hitter that ever lived.";
-			expect(movable.totalPositions()).toEqual(tracker.correct.split(" ").length);
-		});
+	beforeEach(function(){
+		tracker.correct = "There goes Ted Williams, the greatest hitter that ever lived.";
+		tracker.typed = "There goes Ted Wi"
 	});
+
+	it("should define the number of positions as the number of words in the phrase", function(){
+		expect(movable.totalPositions()).toEqual(tracker.correct.split(" ").length);
+	});
+	it("should know the current position", function(){
+		expect(movable.currentPosition()).toEqual(3);
+	});
+	it("should update the image element style based on user progress", function(){
+		var element = document.createElement('img');
+		movable.updateProgress(element);
+		expect(element.style.left).toBe('30%')
+	})
 });
