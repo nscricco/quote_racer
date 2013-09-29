0ed39f0e2276090a0c9e00e8445c605c009a842e
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-28 20:31:24 -0700

    adds game spec: create textbox

--- spec/scripts/gameSpec.js
+++ spec/scripts/gameSpec.js
@@ -12,15 +12,18 @@ describe("game", function(){
 		gameContainer = appendToDom('div', 'gameContainer');
 		gameData = appendToDom('p', 'gameData', gameContainer);
 
-		gameData.innerText = "Wake up the damn bambino and have me face him. I'll drill him in the ass."
+		gameData.innerText = "Wake up the damn bambino and have me face him. I'll drill him in the ass.";
 	});
 
 	describe("on initialize", function(){
 		it("should create a paragraph element with the game phrase", function(){
-			expect(gameContainer.innerHTML).toContain("bambino")
+			expect(gameContainer.innerHTML).toContain("bambino");
 		});
 		it("should return the correct phrase", function(){
 			expect(game.getGameData(gameContainer)).toEqual(gameData.innerText);
+		});
+		it("should create an element in which the user is able to enter text", function(){
+			expect(gameContainer.innerHTML).toContain("button");
 		});
 	});
 });
