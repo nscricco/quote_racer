adc9c8a7bee9acbd10e6d0b71967f37c58b08c00
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-29 11:27:37 -0700

    adds tracker#calcWPM to meet tracker spec

--- app/public/javascripts/accuracyTracker.js
+++ app/public/javascripts/accuracyTracker.js
@@ -6,5 +6,8 @@ var tracker = {
 	},
 	complete: function(){
 		return this.correct.localeCompare(this.typed) == 0;
+	},
+	getWPM: function(){
+		return this.correct.split(" ").length/timer.timeElapsedInMinutes();
 	}
 }
