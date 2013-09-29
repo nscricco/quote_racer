630235bf441efd03bb5c5072ce82ef43d5773177
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-28 18:15:05 -0700

    fixes typo: method missing ()

--- spec/scripts/accuracyTrackerSpec.js
+++ spec/scripts/accuracyTrackerSpec.js
@@ -1,12 +1,16 @@
 describe("tracker", function(){
 	it("should know the correct phrase", function(){
 		expect(tracker.correct).toBeDefined();
-	})
+	});
 	it("should know the typed phrase", function(){
 		expect(tracker.typed).toBeDefined();
-	})
+	});
+	it("should know if the typed phrase is off track", function(){
+		tracker.correct = "Nomar"; tracker.typed = "Nomr";
+		expect(tracker.match()).toBeFalsy();
+	});
 	it("should know if the typed phrase is on track", function(){
-		tracker.correct = "Pedro"; tracker.typed = "Ped"
-		expect(tracker.match).toBeTruthy();
-	})
+		tracker.correct = "Pedro"; tracker.typed = "Ped";
+		expect(tracker.match()).toBeTruthy();
+	});
 });
