3ec6a9d3b4cdbb00bc5207dd96a3db5e735ddc69
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-28 17:15:28 -0700

    adds model spec: require minimum word length for phrase

--- spec/models/data_spec.rb
+++ spec/models/data_spec.rb
@@ -3,8 +3,13 @@ require_relative 'spec_helper'
 describe 'Data' do
 	include Spec
 
+	phrase = GameData.new
+
 	it 'should return a phrase' do
-		phrase = GameData.new
 		expect(phrase.arrested_development).not_to be_empty
+	end
+	it 'should return a phrase of defined minimum length' do
+		phrase_length_in_words = 100
+		expect(phrase.arrested_development.split(" ").length).to be > 100
 	end
 end
