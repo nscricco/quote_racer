d553e1986c0e0e05a22d632565391b91d8f29304
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-29 23:37:02 -0700

    removes typo to deal with highlighter test fails

--- app/public/javascripts/highlighter.js
+++ app/public/javascripts/highlighter.js
@@ -8,7 +8,7 @@ var highlighter = {
 		}
 	},
 	updateWord: function(element){
-		var activeWordRegExp = new RegExp(this.activeWord().replace(/[?]/g, "\\?"));
+		var activeWordRegExp = new RegExp(this.activeWord());
 		element.innerHTML = element.innerText.replace(activeWordRegExp, "<span id=\"highlight\">" + this.activeWord() + "</span>");
 	}
 }
