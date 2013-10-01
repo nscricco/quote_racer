7c552972f8fc11d6fc3b382125da93d79f5f8b6e
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-10-01 01:00:05 -0700

    adds timer spec: should define a finish time

--- spec/scripts/timerSpec.js
+++ spec/scripts/timerSpec.js
@@ -1,5 +1,8 @@
 describe("timer", function(){
 	it("should have a start time", function(){
 		expect(timer.startTime).toBeDefined();
-	})
+	});
+	it("should have a finish time", function(){
+		expect(timer.finishTime).toBeDefined();
+	});
 });
